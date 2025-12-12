5a19e61a039c6b039f8879220ad1735f12dfc7c5
ellaherawati <ellaherwati05@gmail.com>
2025-12-12 14:28:33 +0700

    12 Desember 2025

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ella Herawati - Web Developer Portfolio</title>
+    <link rel="stylesheet" href="assets/css/style.css"></link>
+    <style></style>
+</head>
+
+<body>
+    <header>
+        <h1 class="header-title">I'm Web Developer</h1>
+        <h1 class="header-name">📚Ela Herawati🧤</h1>
+        <p class="header-intro">Hallo! Saya Ela Herawati yang sekarang sedang menempuh pendidikan S1 Informatika dan saya sangat menyukai design dan pemrograman. Ini adalah halaman pertama saya yang saya buat dengan HTML dan CSS. Saya berharap dapat membuat website yang menarik di masa depan.</p>
+        
+        <figure>
+            <img src="instagram/instagram.png" alt="ella's live update" width="950" height="773" loading="lazy">
+            <figcaption><a href="https://www.instagram.com/el.ellaaaa/">Klik Here</a> to follow ella's instagram</figcaption>
+        </figure>
+        
+        <fieldset>
+            <h2>Mau kemana nih?</h2>
+            <nav>
+                <ul>
+                    <li><a href="https://www.instagram.com/el.ellaaaa/" target="_blank">Instagram</a></li>
+                    <li><a href="ellahera.html">Tiktok</a></li>
+                    <li><a href="#aboutme">All About Ella</a></li>
+                </ul>
+            </nav>
+        </fieldset>
+        <hr>
+    </header>
+    
+    <main>
+        <div id="aboutme">
+            <section>
+                <h2 class="about-title">📚About me🥬🥩</h2>
+                <p class="about-subtitle">Sebelum mulai mengenal Ella Lebih dalam, Yuk dengerin dulu lagu yang akan mengingatkanmu ke Ella🧁</p>
+                <video src="video/video.mp4" width="700" height="auto" loop controls poster="tumb/tumb.png"></video>
+            </section>
+        </div>
+        
+        <section>
+            <p class="center-text">Gimana?? Enak kann lagu yang direkomendasiin oleh Ella?</p>
+            <p class="highlight-text">Jangan lupa untuk follow Ella di Instagram untuk update terbaru!</p>
+            <hr><hr>
+            
+            <div class="container">
+                <img src="profileella3/profileella3.JPG" alt="Foto">
+                <p>
+                    <strong class="bio-highlight">Mau lebih jauh kenal aku? Boleh banget dong >.< </strong>
+                    <br>Hallo!! Nama aku Ela Herawati and you can call me Ella📚. Aku lahir di bulan Desember, 2004. Yapsss aku adalah seorang Sagitariuss heheee😋. Kamu percaya zodiak ga?? Aku sih percaya ga percayaa yaaa karnaa banyak banget yang karakternya mirip kayak yang diramal di zodia, tapi banyak juga si yang melesett. Just for fun ajaaaa yaaa heheee karnaaa yang paling penting itu percaya tuhan🌏 Aku suka banget sama design karna dari situ aku bisa mengekspresikan apapun yang aku suka🖼. Tapi design susah juga kalo ga dapet mood nya. Aku juga suka belajar soal cara kerja teknologi dan cara alam semesta bekerja🌋. Dulunya sih maunyaa jadi astronot biar aku bisa ke Saturnus, tapiiii sekarangggg hmmmmmmmmmm🪐🌏 masih mauuu sihhhh. Tapi buat sekarang aku mulai enjoy dengan informatika, ternyata seruuuuuu walupun belajarnya harus super duperrr ngebuttt karna banyak banget yanggg bisa dipelajarii, dan aku ga harus bisa semuanya kann. Tapi aku mau tau tentang bagaimana itu bekerja dan kegunaannya. Tunggu ajaaa, aku bakal buktiin kalo aku bisaaaa menjadi programer yang gajinya dua digit itu😎 Aamiinnnn
+                </p>
+            </div>
+        </section>
+        
+        <hr><hr>
+        
+        <div class="feed-container">
+            <div class="feed-text">
+                <p><strong class="feed-title">Salah satu feed Instagram yang aku suka</strong></p>
+                <p class="feed-description">Aku dari dulu sukaaaaa banget buatttt ngedesign apaaaaapunnnn ituuuu, walaupun masi berantakan🍰. Tapi kayakk kalo liat halaman kosong tuhhhh kurangggg bangetttttt😄. Pokoknya semua harus warna warni🦋📚. Pas masuk jurusan multimediaa, awal awal struggleee sihhh karnaaaa aku orangnya emang binggunagan banget di awal awal🍷. Tapi pas udah terjun dannnn udah mulai paham, hmmmmm asik juga main main sama tools😋.</p>
+            </div>
+            <img src="instagram2/instagram2.png" alt="feed" width="500" height="auto">
+        </div>
+        
+        <br><br><br>
+        
+        <h1 class="gallery-title">🍰Feed Akuuuuuuu📚</h1>
+        <div class="gallery">
+            <img src="foto1/foto1.png" alt="Foto 1" height="350">
+            <img src="foto2/foto2.png" alt="Foto 2" height="350">
+            <img src="foto3/foto3.png" alt="Foto 3" height="350">
+        </div>
+        
+        <h1 class="portfolio-title">Ella's Portofolio</h1>
+        <img src="foto4/portofolio.png" alt="portofolio" class="portfolio-img" width="900" height="auto">
+        
+        <div class="map-container">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3954.7803216904363!2d110.81411299999999!3d-7.598870699999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e7a17917a011ce7%3A0x46ccabdfaa68d501!2sUniversitas%20Sugeng%20Hartono%20(USH)!5e0!3m2!1sen!2sid!4v1748013260405!5m2!1sen!2sid" width="1000" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            <p class="address-text">Saya adalah seorang mahasiswi semester 2 di Universitas Sugeng Hartono. Bagi kalian yang ingin mengenal saya lebih jauh, silakan kunjungi kampus saya di alamat berikut:
+                <strong class="address-highlight">Jl. Ir. Soekarno No.69, Dusun I, Madegondo, Kec. Grogol, Kabupaten Sukoharjo, Jawa Tengah 57552</strong>
+            </p>
+        </div>
+
+        <div class="form-container">
+            <fieldset>
+                <legend>✏️Mau tau donggggg📚</legend>
+                <ul>
+                    <li><h3 class="form-question">Apakah kamu penah mendengar tentang Universitas Sugeng Hartono sebelumnya?</h3></li>
+                    <p><input type="radio" name="maps" id="pernah" value="pernah">
+                        <label for="pernah" style="color: rgb(255, 91, 165);">Pernah</label></p>
+                    <p><input type="radio" name="maps" id="tidakpernah" value="tidakpernah">
+                        <label for="tidakpernah" style="color: rgb(255, 91, 165);">Tidak Pernah</label></p>
+                    
+                    <li><h3 class="form-question">Dari mana kamu tau tentang Universitas Sugeng Hartono?</h3></li>
+                    <p><input type="checkbox" name="sumber" id="instagram" value="instagram">
+                        <label for="instagram">Instagram</label></p>
+                    <p><input type="checkbox" name="sumber" id="tiktok" value="tiktok">
+                        <label for="tiktok">Tik Tok</label></p>
+                    <p><input type="checkbox" name="sumber" id="brosur" value="brosur">
+                        <label for="brosur">Brosur</label></p>
+                    <p><input type="checkbox" name="sumber" id="Expo" value="Expo">
+                        <label for="Expo">Expo Kampus</label></p>
+                    <p><input type="checkbox" name="sumber" id="lainnya" value="lainnya">
+                        <label for="lainnya">Lainnya</label></p>
+                    <br><button type="submit">Kirim</button>
+                </ul>
+            </fieldset>
+        </div>
+        <hr><hr>
+    </main>
+    
+    <footer>
+        <p>© 2024 Ela Herawati. Made with 💕</p>
+    </footer>
+</body>
+</html>
